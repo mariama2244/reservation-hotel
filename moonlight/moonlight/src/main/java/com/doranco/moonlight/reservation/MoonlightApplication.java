ac9936f70ea4e765a4bcb383dbbe84fb9caa16b9
mariama2244 <119751878+mariama2244@users.noreply.github.com>
2024-04-24 14:09:33 +0200

    spring dossier 24/04.2024

---
+++ moonlight/moonlight/src/main/java/com/doranco/moonlight/reservation/MoonlightApplication.java
@@ -0,0 +1,13 @@
+package com.doranco.moonlight.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonlightApplication.class, args);
+	}
+
+}
